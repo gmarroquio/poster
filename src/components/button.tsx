84c066cd526dfb226adc4a1fbd711a41b97704c6
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-04-24 17:39:34 -0300

    add loading state to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,17 +1,26 @@
+"use client";
+import { LoaderIcon } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
-export const Button: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export const Button: React.FC<{
+  children: React.ReactNode;
+  disabled?: boolean;
+}> = ({ children, disabled = false }) => {
   const { pending } = useFormStatus();
 
   return (
     <button
-      type="submit"
-      className="bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4 cursor-pointer"
-      disabled={pending}
+      className="disabled:bg-gray-400 disabled:cursor-default bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4 cursor-pointer"
+      disabled={pending || disabled}
     >
-      {children}
+      {pending ? (
+        <div className="flex items-center justify-center space-x-2">
+          <LoaderIcon className="animate-spin h-5 w-5" />
+          <span>Loading</span>
+        </div>
+      ) : (
+        children
+      )}
     </button>
   );
 };
